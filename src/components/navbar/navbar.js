105fbba6352d340b7295550744a489c24ed1ff5b
p3jit <prithi.das.007@gmail.com>
2022-08-14 03:36:44 +0530

    navbar done 1

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import "./navbar.css"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { styled } from '@mui/material/styles';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import Badge from '@mui/material/Badge';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: 2,
+      top: 8,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }));
 
 function Navbar() {
   return (
@@ -10,12 +23,17 @@ function Navbar() {
             </div>
             <div className='search-container'>
                 <input className='search-bar'/>
+                <button className='search-btn'><SearchOutlinedIcon/></button>
             </div>
             <div className='cart-account-container'>
-                <div className='account-container'>Register/Sign in</div>
-                <div className='cart-container'>Cart</div>
-            </div>
-            
+                <div className='account-link'>REGISTER/SIGN IN</div>
+                <div className='cart-container'>
+                    <div className='cart-txt'>CART</div>
+                    <StyledBadge badgeContent={1} color="primary">
+                        <button className='cart-btn'><ShoppingBagOutlinedIcon/></button>
+                    </StyledBadge>
+                </div>
+            </div>   
         </div>
     </>
   )
