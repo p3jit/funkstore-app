d32b7653ab6bd6ea7f9bb3c3b1c016d302f02700
p3jit <prithi.das.007@gmail.com>
2022-08-21 03:54:10 +0530

    updated login fetch

--- src/pages/login.js
+++ src/pages/login.js
@@ -41,7 +41,7 @@ function Login() {
 
     //Login function
     const fetchUser = async () => {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}api/auth/login` , {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}auth/login` , {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
